4b5834cf403f1e8268facbd4676da8e7c0f2b952
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2024-01-19 22:31:03 +0000

    added routing and beginning of signup

--- backend/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// Define a GORM model
-type User struct {
-	ID    uint   `gorm:"primaryKey"`
-	Name  string `gorm:"not null"`
-	Phone string `gorm:"not null"`
-	Email string `gorm:"not null"`
-}
